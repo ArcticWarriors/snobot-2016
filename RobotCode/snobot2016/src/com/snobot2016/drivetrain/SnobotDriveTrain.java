dbea6db7d2ff3b14afd6381487eb0cb0b43d9aa7
Ayush Goel <goelayush@mylcsd.org>
2016-01-16 15:15:50 -0500

    Added Smart Dashboard Stuff

--- RobotCode/snobot2016/src/com/snobot2016/drivetrain/SnobotDriveTrain.java
+++ RobotCode/snobot2016/src/com/snobot2016/drivetrain/SnobotDriveTrain.java
@@ -1,9 +1,11 @@
 package com.snobot2016.drivetrain;
 
+import com.snobot2016.SmartDashBoardNames;
 import com.snobot2016.joystick.IDriverJoystick;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SnobotDriveTrain implements IDriveTrain
 {
@@ -55,7 +57,13 @@ public class SnobotDriveTrain implements IDriveTrain
     public void updateSmartDashboard()
     {
         // TODO Auto-generated method stub
+        SmartDashboard.putNumber(SmartDashBoardNames.sLEFT_DRIVE_MOTOR_ENCODER, mLeftEncoder.getDistance());
 
+        SmartDashboard.putNumber(SmartDashBoardNames.sRIGHT_DRIVE_MOTOR_ENCODER, mRightEncoder.getDistance());
+
+        SmartDashboard.putNumber(SmartDashBoardNames.sLEFT_DRIVE_MOTOR_SPEED, mLeftMotor.get());
+
+        SmartDashboard.putNumber(SmartDashBoardNames.sRIGHT_DRIVE_MOTOR_SPEED, mRightMotor.get());
     }
 
     @Override
