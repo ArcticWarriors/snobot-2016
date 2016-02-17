5c508838b25a2b53161a9102663b09da2006ef78
PJ Reiniger <pj.reiniger@gmail.com>
2016-02-16 19:34:22 -0500

    Updating the simulator to deal with the eventual fact that the encoders will be coming through the can talon

--- Simulator/Simulator/src/com/snobot/simulator/robot_sim/Snobot2016Simulator.java
+++ Simulator/Simulator/src/com/snobot/simulator/robot_sim/Snobot2016Simulator.java
@@ -14,112 +14,129 @@ public class Snobot2016Simulator extends ASimulator
 {
     public Snobot2016Simulator()
     {
-        SpeedControllerWrapper leftDriveMotor;
-        SpeedControllerWrapper rightDriveMotor;
-
-        // Speed Controllers
-        if (Properties2016.sIS_REAL_ROBOT)
+        //////////////////////////////
+        // Drivetrain
+        //////////////////////////////
         {
-            leftDriveMotor = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_LEFT_A_PORT);
-            rightDriveMotor = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_RIGHT_A_PORT);
+            double drivetrainSpeed = 3.8 * 12;
+            AnalogWrapper gyro;
 
-            SpeedControllerWrapper leftDriveMotorB = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_LEFT_B_PORT);
-            SpeedControllerWrapper rightDriveMotorB = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_RIGHT_B_PORT);
+            if (Properties2016.sUSE_SPI_GYRO)
+            {
+                gyro = SensorActuatorRegistry.get().getAnalog().get(100);
+            }
+            else
+            {
+                gyro = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sGYRO_SENSOR_PORT);
+            }
+            gyro.setName("Gyro");
+            
+            if (Properties2016.sIS_REAL_ROBOT)
+            {
+                SpeedControllerWrapper leftDriveMotor = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_LEFT_A_PORT);
+                SpeedControllerWrapper rightDriveMotor = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_RIGHT_A_PORT);
+                SpeedControllerWrapper leftDriveMotorB = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_LEFT_B_PORT);
+                SpeedControllerWrapper rightDriveMotorB = SensorActuatorRegistry.get().getCanSpeedControllers().get(Properties2016.sDRIVE_CAN_RIGHT_B_PORT);
 
-            leftDriveMotor.setName("Left Drive (A)");
-            leftDriveMotorB.setName("Left Drive (B)");
-            rightDriveMotor.setName("Right Drive (A)");
-            rightDriveMotorB.setName("Right Drive (B)");
+                leftDriveMotor.setName("Left Drive (A)");
+                leftDriveMotorB.setName("Left Drive (B)");
+                rightDriveMotor.setName("Right Drive (A)");
+                rightDriveMotorB.setName("Right Drive (B)");
+
+                leftDriveMotor.setMotorParameters(drivetrainSpeed);
+                rightDriveMotor.setMotorParameters(-drivetrainSpeed);
+            }
+            else
+            {
+                SpeedControllerWrapper leftDriveMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sDRIVER_LEFT_MOTOR_PORT);
+                SpeedControllerWrapper rightDriveMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sDRIVER_RIGHT_MOTOR_PORT);
+                EncoderWrapper leftDriveEncoder = SensorActuatorRegistry.get().getEncoder(Properties2016.sLEFT_DRIVE_ENCODER_PORT_A, Properties2016.sLEFT_DRIVE_ENCODER_PORT_B);
+                EncoderWrapper rightDriveEncoder = SensorActuatorRegistry.get().getEncoder(Properties2016.sRIGHT_DRIVE_ENCODER_PORT_A, Properties2016.sRIGHT_DRIVE_ENCODER_PORT_B);
+
+                leftDriveMotor.setName("Left Drive");
+                rightDriveMotor.setName("Right Drive");
+                leftDriveEncoder.setName("Left Drive");
+                rightDriveEncoder.setName("Right Drive");
+
+                leftDriveEncoder.setSpeedController(leftDriveMotor);
+                rightDriveEncoder.setSpeedController(rightDriveMotor);
+                leftDriveEncoder.setDistancePerTick(Properties2016.sLEFT_ENCODER_DIST_PER_PULSE.getValue());
+                rightDriveEncoder.setDistancePerTick(Properties2016.sRIGHT_ENCODER_DIST_PER_PULSE.getValue());
+
+                leftDriveMotor.setMotorParameters(drivetrainSpeed);
+                rightDriveMotor.setMotorParameters(-drivetrainSpeed);
+
+                TankDriveGyroSimulator gyroSim = new TankDriveGyroSimulator(leftDriveEncoder, rightDriveEncoder, gyro);
+                mSimulatorComponenets.add(gyroSim);
+
+            }
         }
-        else
+        
+        
+        //////////////////////////////
+        // Scaler
+        //////////////////////////////
         {
-            leftDriveMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sDRIVER_LEFT_MOTOR_PORT);
-            rightDriveMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sDRIVER_RIGHT_MOTOR_PORT);
+            SpeedControllerWrapper scaleLiftMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sSCALE_MOVE_MOTOR_PORT);
+            SpeedControllerWrapper scaleTiltMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sSCALE_TILT_MOTOR_PORT);
 
-            leftDriveMotor.setName("Left Drive");
-            rightDriveMotor.setName("Right Drive");
+            AnalogWrapper scaleTiltPot = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sSCALE_POT_PORT);
+            AnalogWrapper scaleLiftPot = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sEXTENSION_POT_PORT);
+
+            scaleLiftMotor.setName("Scale (Lift)");
+            scaleTiltMotor.setName("Scale (Tilt)");
+            scaleTiltPot.setName("Scale Pot");
+            scaleLiftPot.setName("Scale Lift");
+
+            scaleTiltMotor.setMotorParameters(30); // Degrees / second
+            scaleTiltMotor.setPosition(30);
+            scaleLiftMotor.setMotorParameters(10);
+
+            PotentiometerSimulator scaleTiltPotSim = new PotentiometerSimulator(scaleTiltPot, scaleTiltMotor);
+            mSimulatorComponenets.add(scaleTiltPotSim);
+
+            double scalePotThrow = Properties2016.sSCALE_HIGH_ANGLE.getValue() - Properties2016.sSCALE_LOW_ANGLE.getValue();
+            double scalePotMinVoltage = Properties2016.sSCALE_LOW_VOLTAGE.getValue();
+            double scalePotMaxVoltage = Properties2016.sSCALE_HIGH_VOLTAGE.getValue();
+            scaleTiltPotSim.setParameters(scalePotThrow, scalePotMinVoltage, scalePotMaxVoltage);
+
+            // Scaling Lifting Potentiometer
+            PotentiometerSimulator scaleLiftPotSim = new PotentiometerSimulator(scaleLiftPot, scaleLiftMotor);
+            mSimulatorComponenets.add(scaleLiftPotSim);
+
+            double scaleLiftPotMinVoltage = Properties2016.sMIN_SCALE_EXTENSION_POT_VOLTAGE.getValue();
+            double scaleLiftPotMaxVoltage = Properties2016.sMAX_SCALE_EXTENSION_POT_VOLTAGE.getValue();
+            scaleLiftPotSim.setParameters(100, scaleLiftPotMinVoltage, scaleLiftPotMaxVoltage);
         }
-        SpeedControllerWrapper scaleLiftMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sSCALE_MOVE_MOTOR_PORT);
-        SpeedControllerWrapper scaleTiltMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sSCALE_TILT_MOTOR_PORT);
-        SpeedControllerWrapper intakeMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sHARVESTER_ROLLER_MOTOR_PORT);
-        SpeedControllerWrapper intakeTiltMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sHARVESTER_PIVOT_MOTOR_PORT);
 
-        scaleLiftMotor.setName("Scale (Lift)");
-        scaleTiltMotor.setName("Scale (Tilt)");
-        intakeMotor.setName("Intake (Roller)");
-        intakeTiltMotor.setName("Intake (Tilt)");
-
-        // Encoders
-        EncoderWrapper leftDriveEncoder = SensorActuatorRegistry.get().getEncoder(Properties2016.sLEFT_DRIVE_ENCODER_PORT_A,
-                Properties2016.sLEFT_DRIVE_ENCODER_PORT_B);
-        EncoderWrapper rightDriveEncoder = SensorActuatorRegistry.get().getEncoder(Properties2016.sRIGHT_DRIVE_ENCODER_PORT_A,
-                Properties2016.sRIGHT_DRIVE_ENCODER_PORT_B);
-        leftDriveEncoder.setName("Left Drive");
-        rightDriveEncoder.setName("Right Drive");
-        leftDriveEncoder.setSpeedController(leftDriveMotor);
-        rightDriveEncoder.setSpeedController(rightDriveMotor);
-        leftDriveEncoder.setDistancePerTick(Properties2016.sLEFT_ENCODER_DIST_PER_PULSE.getValue());
-        rightDriveEncoder.setDistancePerTick(Properties2016.sRIGHT_ENCODER_DIST_PER_PULSE.getValue());
-
-        // Relay
-        RelayWrapper lightRelay = SensorActuatorRegistry.get().getRelays().get(Properties2016.sLIGHT_RELAY);
-        lightRelay.setName("Cam. Light");
-
-        // Analaog
-        AnalogWrapper gyro;
-
-        if (Properties2016.sUSE_SPI_GYRO)
+        //////////////////////////////
+        // Intake
+        //////////////////////////////
         {
-            gyro = SensorActuatorRegistry.get().getAnalog().get(100);
+            SpeedControllerWrapper intakeMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sHARVESTER_ROLLER_MOTOR_PORT);
+            SpeedControllerWrapper intakeTiltMotor = SensorActuatorRegistry.get().getSpeedControllers().get(Properties2016.sHARVESTER_PIVOT_MOTOR_PORT);
+            AnalogWrapper intakePot = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sHARVESTER_POT_PORT);
+            PotentiometerSimulator intakePotSim = new PotentiometerSimulator(intakePot, intakeTiltMotor);
+
+            intakePot.setName("Intake Pot");
+            intakeMotor.setName("Intake (Roller)");
+            intakeTiltMotor.setName("Intake (Tilt)");
+            intakeTiltMotor.setMotorParameters(120);
+
+            double intakePotMinVoltage = Properties2016.sMIN_HARVESTER_POT_VOLTAGE.getValue();
+            double intakePotMaxVoltage = Properties2016.sMAX_HARVESTER_POT_VOLTAGE.getValue();
+            intakePotSim.setParameters(100, intakePotMinVoltage, intakePotMaxVoltage);
+
+            mSimulatorComponenets.add(intakePotSim);
         }
-        else
+
+        //////////////////////////////
+        // Light
+        //////////////////////////////
         {
-            gyro = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sGYRO_SENSOR_PORT);
+            RelayWrapper lightRelay = SensorActuatorRegistry.get().getRelays().get(Properties2016.sLIGHT_RELAY);
+            lightRelay.setName("Cam. Light");
         }
-        AnalogWrapper scaleTiltPot = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sSCALE_POT_PORT);
-        AnalogWrapper scaleLiftPot = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sEXTENSION_POT_PORT);
-        AnalogWrapper intakePot = SensorActuatorRegistry.get().getAnalog().get(Properties2016.sHARVESTER_POT_PORT);
-        gyro.setName("Gyro");
-        scaleTiltPot.setName("Scale Pot");
-        scaleLiftPot.setName("Scale Lift");
-        intakePot.setName("Intake Pot");
-
-        // Set Parameters
-        double drivetrainSpeed = 3.8 * 12;
-        leftDriveMotor.setMotorParameters(drivetrainSpeed);
-        rightDriveMotor.setMotorParameters(-drivetrainSpeed);
-        scaleTiltMotor.setMotorParameters(30); // Degrees / second
-        scaleTiltMotor.setPosition(30);
-        intakeTiltMotor.setMotorParameters(120);
-        scaleLiftMotor.setMotorParameters(10);
-
-        TankDriveGyroSimulator gyroSim = new TankDriveGyroSimulator(leftDriveEncoder, rightDriveEncoder, gyro);
-        mSimulatorComponenets.add(gyroSim);
-
-        // Scaling Potentiometer
-        PotentiometerSimulator scaleTiltPotSim = new PotentiometerSimulator(scaleTiltPot, scaleTiltMotor);
-        mSimulatorComponenets.add(scaleTiltPotSim);
-
-        double scalePotThrow = Properties2016.sSCALE_HIGH_ANGLE.getValue() - Properties2016.sSCALE_LOW_ANGLE.getValue();
-        double scalePotMinVoltage = Properties2016.sSCALE_LOW_VOLTAGE.getValue();
-        double scalePotMaxVoltage = Properties2016.sSCALE_HIGH_VOLTAGE.getValue();
-        scaleTiltPotSim.setParameters(scalePotThrow, scalePotMinVoltage, scalePotMaxVoltage);
-
-        // Scaling Lifting Potentiometer
-        PotentiometerSimulator scaleLiftPotSim = new PotentiometerSimulator(scaleLiftPot, scaleLiftMotor);
-        mSimulatorComponenets.add(scaleLiftPotSim);
-
-        double scaleLiftPotMinVoltage = Properties2016.sMIN_SCALE_EXTENSION_POT_VOLTAGE.getValue();
-        double scaleLiftPotMaxVoltage = Properties2016.sMAX_SCALE_EXTENSION_POT_VOLTAGE.getValue();
-        scaleLiftPotSim.setParameters(100, scaleLiftPotMinVoltage, scaleLiftPotMaxVoltage);
-
-        // Harvester Potentiometer
-        PotentiometerSimulator intakePotSim = new PotentiometerSimulator(intakePot, intakeTiltMotor);
-        mSimulatorComponenets.add(intakePotSim);
-
-        double intakePotMinVoltage = Properties2016.sMIN_HARVESTER_POT_VOLTAGE.getValue();
-        double intakePotMaxVoltage = Properties2016.sMAX_HARVESTER_POT_VOLTAGE.getValue();
-        intakePotSim.setParameters(100, intakePotMinVoltage, intakePotMaxVoltage);
     }
 
 }
