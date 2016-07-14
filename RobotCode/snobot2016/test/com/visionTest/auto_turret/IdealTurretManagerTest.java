a509a62e35d94a8691f88a6af069f30e8aa9ae8f
PJ Reiniger <pj.reiniger@gmail.com>
2016-07-13 20:30:19 -0400

    Forgot to add the unit tests

---
+++ RobotCode/snobot2016/test/com/visionTest/auto_turret/IdealTurretManagerTest.java
@@ -0,0 +1,103 @@
+package com.visionTest.auto_turret;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class IdealTurretManagerTest
+{
+    private IStateManager mStateManager;
+
+    public IdealTurretManagerTest()
+    {
+        mStateManager = new StateManager();
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void temp()
+    {
+        System.out.println("\n\nStarting Test:" + name.getMethodName());
+        System.out.println("------------------------------------------------------------");
+    }
+
+    @Test
+    public void testHistoryManager()
+    {
+        double timestamp0 = 1234;
+        double timestamp0_5 = 1235;
+        double timestamp1 = 1236;
+        double timestamp2 = 1238;
+
+        double robot_x = 1;
+        double robot_y = 2;
+        double robot_angle = 45;
+        double turret_angle = 20;
+        IStateManager.RobotState state_at_time;
+
+        // Nothing has been filled in yet, we should expect invalid data
+        Assert.assertFalse(mStateManager.canCalculate(timestamp0));
+
+        // Add data for t0
+        mStateManager.saveRobotState(timestamp0, robot_x, robot_y, robot_angle, turret_angle);
+
+        Assert.assertTrue(mStateManager.canCalculate(timestamp0));
+        state_at_time = mStateManager.getStateHistory(timestamp0);
+        Assert.assertNotNull(state_at_time);
+        Assert.assertEquals(1, state_at_time.mRobotX, 0);
+        Assert.assertEquals(2, state_at_time.mRobotY, 0);
+        Assert.assertEquals(65, state_at_time.mAbsoluteAngle, 0);
+
+        // At t1, we have not updated the state, so we should get the same value
+        Assert.assertTrue(mStateManager.canCalculate(timestamp1));
+        state_at_time = mStateManager.getStateHistory(timestamp1);
+        Assert.assertNotNull(state_at_time);
+        Assert.assertEquals(1, state_at_time.mRobotX, 0);
+        Assert.assertEquals(2, state_at_time.mRobotY, 0);
+        Assert.assertEquals(65, state_at_time.mAbsoluteAngle, 0);
+
+        // Add data for t1
+        robot_x = 123;
+        robot_y = 321;
+        robot_angle = -50;
+        turret_angle = 30;
+        mStateManager.saveRobotState(timestamp1, robot_x, robot_y, robot_angle, turret_angle);
+
+        // Got data between t0 and t1... I want to "round down" and get the older data
+        Assert.assertTrue(mStateManager.canCalculate(timestamp0_5));
+        state_at_time = mStateManager.getStateHistory(timestamp0_5);
+        Assert.assertNotNull(state_at_time);
+        Assert.assertEquals(1, state_at_time.mRobotX, 0);
+        Assert.assertEquals(2, state_at_time.mRobotY, 0);
+        Assert.assertEquals(65, state_at_time.mAbsoluteAngle, 0);
+
+        // Got data at t1
+        Assert.assertTrue(mStateManager.canCalculate(timestamp1));
+        state_at_time = mStateManager.getStateHistory(timestamp1);
+        Assert.assertNotNull(state_at_time);
+        Assert.assertEquals(123, state_at_time.mRobotX, 0);
+        Assert.assertEquals(321, state_at_time.mRobotY, 0);
+        Assert.assertEquals(-20, state_at_time.mAbsoluteAngle, 0);
+
+        // Got data before we before we updated the state (t2)
+        Assert.assertTrue(mStateManager.canCalculate(timestamp2));
+        state_at_time = mStateManager.getStateHistory(timestamp2);
+        Assert.assertNotNull(state_at_time);
+        Assert.assertEquals(123, state_at_time.mRobotX, 0);
+        Assert.assertEquals(321, state_at_time.mRobotY, 0);
+        Assert.assertEquals(-20, state_at_time.mAbsoluteAngle, 0);
+
+        // Got a retrograde update
+        Assert.assertTrue(mStateManager.canCalculate(timestamp0));
+        state_at_time = mStateManager.getStateHistory(timestamp0);
+        Assert.assertNotNull(state_at_time);
+        Assert.assertEquals(1, state_at_time.mRobotX, 0);
+        Assert.assertEquals(2, state_at_time.mRobotY, 0);
+        Assert.assertEquals(65, state_at_time.mAbsoluteAngle, 0);
+    }
+
+}
---
+++ RobotCode/snobot2016/test/com/visionTest/auto_turret/StateMangerTest_SimpleAngles.java
@@ -0,0 +1,191 @@
+package com.visionTest.auto_turret;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class StateMangerTest_SimpleAngles
+{
+    private static double sEPSILON = .001;
+
+    private IStateManager mStateManager;
+
+    public StateMangerTest_SimpleAngles()
+    {
+        mStateManager = new StateManager();
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void temp()
+    {
+        System.out.println("\n\nStarting Test:" + name.getMethodName());
+        System.out.println("------------------------------------------------------------");
+    }
+
+    @Test
+    /**
+     * - Robot to the left of the y-axis, pointing straight ahead.
+     * - Camera sees the target at 45 degrees, 14.14 feet away ([10, 10] away, at [0, 10])
+     * - Camera update happens "immediately"
+     * 
+     * Expect the desired angle to match the cameras offset
+     * 
+     */
+    public void testRobotAngle0_TurretAngle0_Calculation0()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, -10, 0, 0, 0);
+        mStateManager.setCurrentCameraState(timestamp, 45, 14.1421356237);
+
+        Assert.assertEquals(0, mStateManager.getTargetLocation().mX, sEPSILON);
+        Assert.assertEquals(10, mStateManager.getTargetLocation().mY, sEPSILON);
+        Assert.assertEquals(45, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * - Robot to the right of the y-axis, pointing straight ahead.
+     * - Camera sees the target at -45 degrees, 14.14 feet away ([-10, 10] away, at [0, 10]))
+     * - Camera update happens "immediately"
+     * 
+     * Expect the desired angle to match the cameras offset
+     * 
+     */
+    public void testRobotAngle0_TurretAngle0_Calculation1()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, 10, 0, 0, 0);
+        mStateManager.setCurrentCameraState(timestamp, -45, 14.1421356237);
+
+        Assert.assertEquals(0, mStateManager.getTargetLocation().mX, sEPSILON);
+        Assert.assertEquals(10, mStateManager.getTargetLocation().mY, sEPSILON);
+        Assert.assertEquals(-45, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngle0_TurretAngle0_Calculation2()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, -5, 0, 0, 0);
+        mStateManager.setCurrentCameraState(timestamp, 30, 10);
+
+        Assert.assertEquals(0.00, mStateManager.getTargetLocation().mX, sEPSILON);
+        Assert.assertEquals(8.66, mStateManager.getTargetLocation().mY, sEPSILON);
+        Assert.assertEquals(30, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngle0_TurretAngle0_Calculation3()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, 5, 0, 0, 0);
+        mStateManager.setCurrentCameraState(timestamp, -30, 10);
+
+        Assert.assertEquals(0.00, mStateManager.getTargetLocation().mX, sEPSILON);
+        Assert.assertEquals(8.66, mStateManager.getTargetLocation().mY, sEPSILON);
+        Assert.assertEquals(-30, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngle0_TurretAngle0_Calculation4()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, -10, 0, 0, 0);
+        mStateManager.setCurrentCameraState(timestamp, 60, 11.547);
+
+        Assert.assertEquals(0.000, mStateManager.getTargetLocation().mX, sEPSILON);
+        Assert.assertEquals(5.773, mStateManager.getTargetLocation().mY, sEPSILON);
+        Assert.assertEquals(60, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngle0_TurretAngle0_Calculation5()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, 10, 0, 0, 0);
+        mStateManager.setCurrentCameraState(timestamp, -60, 11.547);
+
+        Assert.assertEquals(0.000, mStateManager.getTargetLocation().mX, sEPSILON);
+        Assert.assertEquals(5.773, mStateManager.getTargetLocation().mY, sEPSILON);
+        Assert.assertEquals(-60, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngleSkewed_TurretAngle0_Calculation0()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, -10, 0, 45, 0);
+        mStateManager.setCurrentCameraState(timestamp, 0, 14.1421356237);
+
+        Assert.assertEquals(45, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngleSkewed_TurretAngle0_Calculation1()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, 5, 0, -30, 0);
+        mStateManager.setCurrentCameraState(timestamp, 0, 10);
+
+        Assert.assertEquals(-30, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngle0_TurretAngleSkewed_Calculation0()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, -10, 0, 0, 45);
+        mStateManager.setCurrentCameraState(timestamp, 0, 14.1421356237);
+
+        Assert.assertEquals(45, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+
+    @Test
+    /**
+     * 
+     */
+    public void testRobotAngle0_TurretAngleSkewed_Calculation1()
+    {
+        double timestamp = 0;
+
+        mStateManager.saveRobotState(timestamp, 5, 0, 0, -30);
+        mStateManager.setCurrentCameraState(timestamp, 0, 10);
+
+        Assert.assertEquals(-30, mStateManager.calculateDesiredAngleFromState(), sEPSILON);
+    }
+}
