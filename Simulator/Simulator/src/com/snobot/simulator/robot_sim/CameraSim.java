2f12b2903bed11c1648e267c49da4c10d3bef4f0
PJ Reiniger <pj.reiniger@gmail.com>
2016-07-17 15:24:01 -0400

    Adding empty camerasim so the project compiles

---
+++ Simulator/Simulator/src/com/snobot/simulator/robot_sim/CameraSim.java
@@ -0,0 +1,25 @@
+package com.snobot.simulator.robot_sim;
+
+import com.snobot.simulator.ISimulatorUpdater;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class CameraSim implements ISimulatorUpdater
+{
+    public CameraSim()
+    {
+
+    }
+
+    @Override
+    public void update()
+    {
+    }
+
+    @Override
+    public void setRobot(RobotBase aRobot)
+    {
+
+    }
+
+}
